d00b60b66f41251d58a0a702a27d66bfe345a033
TomDeeterink1 <deeterinktom@gmail.com>
2023-09-18 10:00:26 +0200

    push
    push

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const button = document.querySelector(".profile-button");
+button.addEventListener("mouseover", getmenu);
 
+const hiddencontent = document.querySelector(".hidden");
+
+function getmenu() {
+    hiddencontent.classList.toggle('visible')
+}
