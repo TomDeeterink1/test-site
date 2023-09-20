ba90f1e1128cadab23be58a29cee2ea4376bcee3
TomDeeterink1 <deeterinktom@gmail.com>
2023-09-20 16:54:08 +0200

    commit

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ function getmenu(i) {
 
     
 }
+
+const articles = document.querySelectorAll(".header-series");
+let currentIndex = 0;
+
+const articleDisplay = document.querySelector(".content");
+const nextButton = document.querySelector(".next");
+const backButton = document.querySelector(".back");
+
+nextButton.addEventListener("click", showNextArticle);
+backButton.addEventListener("click", showPreviousArticle);
+
+function showNextArticle() {
+  articles[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % articles.length;
+  articles[currentIndex].classList.add("active");
+}
+
+function showPreviousArticle() {
+  articles[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex - 1 + articles.length) % articles.length;
+  articles[currentIndex].classList.add("active");
+}
+
+// Initialize the display with the first article
+articles[currentIndex].classList.add("active");
+
+
+
